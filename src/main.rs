85b77a8d17a24378fbbe9caab132c5d3daa9c2ab
Gustavo Baptista <gustavogsb@outlook.com>
2025-10-30 11:12:23 -0300

    chore: initialize Rust project with Cargo configuration and main application file

---
+++ src/main.rs
@@ -0,0 +1,4 @@
+fn main() {
+    println!("Hello, U.are.U Rust!");
+    println!("A Rust library designed to enable communication between a Rust application and the DLL/OS of the DPFJ and DPFPDD libraries created by DigitalPersona/HID Global.");
+}
